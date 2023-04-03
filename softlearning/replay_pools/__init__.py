ec19550bec3be57c1e96cabb6b2ec23676be0edb
Yao Yao <474127934@qq.com>
2023-04-03 14:21:37 +0800

    Update __init__.py

--- softlearning/replay_pools/__init__.py
+++ softlearning/replay_pools/__init__.py
@@ -1,4 +1,4 @@
-from .simple_replay_pool import SimpleReplayPool
+from .simple_replay_pool import SimpleReplayPool, WeightedReplayPool
 from .extra_policy_info_replay_pool import ExtraPolicyInfoReplayPool
 from .union_pool import UnionPool
 from .trajectory_replay_pool import TrajectoryReplayPool
