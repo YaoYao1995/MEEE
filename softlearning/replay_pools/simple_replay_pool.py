bf6095a209446a7add48aaf64c7e3819eae99c9c
Yao Yao <474127934@qq.com>
2023-04-03 13:58:43 +0800

    Update simple_replay_pool.py

--- softlearning/replay_pools/simple_replay_pool.py
+++ softlearning/replay_pools/simple_replay_pool.py
@@ -69,7 +69,7 @@ class SimpleReplayPool(FlexibleReplayPool):
 
         super(SimpleReplayPool, self).__init__(
             *args, fields_attrs=fields, **kwargs)
-
+                  
     def add_samples(self, samples):
         if not isinstance(self._observation_space, Dict):
             return super(SimpleReplayPool, self).add_samples(samples)
@@ -169,3 +169,116 @@ class SimpleReplayPool(FlexibleReplayPool):
 
     def terminate_episode(self):
         pass
+
+class WeightedReplayPool(FlexibleReplayPool):
+    def __init__(self, observation_space, action_space, *args, **kwargs):
+        self._observation_space = observation_space
+        self._action_space = action_space
+
+        observation_fields = normalize_observation_fields(observation_space)
+        # It's a bit memory inefficient to save the observations twice,
+        # but it makes the code *much* easier since you no longer have
+        # to worry about termination conditions.
+        observation_fields.update({
+            'next_' + key: value
+            for key, value in observation_fields.items()
+        })
+
+        fields = {
+            **observation_fields,
+            **{
+                'actions': {
+                    'shape': self._action_space.shape,
+                    'dtype': 'float32'
+                },
+                'rewards': {
+                    'shape': (1, ),
+                    'dtype': 'float32'
+                },
+                # self.terminals[i] = a terminal was received at time i
+                'terminals': {
+                    'shape': (1, ),
+                    'dtype': 'bool'
+                },
+                # uncertainty of the tuple
+                'stds': {
+                    'shape': (1, ),
+                    'dtype': 'float32'
+                },
+            }
+        }
+
+        super(WeightedReplayPool, self).__init__(
+            *args, fields_attrs=fields, **kwargs)
+
+    def add_samples(self, samples):
+        if not isinstance(self._observation_space, Dict):
+            return super(WeightedReplayPool, self).add_samples(samples)
+
+        dict_observations = defaultdict(list)
+        for observation in samples['observations']:
+            for key, value in observation.items():
+                dict_observations[key].append(value)
+
+        dict_next_observations = defaultdict(list)
+        for next_observation in samples['next_observations']:
+            for key, value in next_observation.items():
+                dict_next_observations[key].append(value)
+
+        samples.update(
+           **{
+               f'observations.{observation_key}': np.array(values)
+               for observation_key, values in dict_observations.items()
+           },
+           **{
+               f'next_observations.{observation_key}': np.array(values)
+               for observation_key, values in dict_next_observations.items()
+           },
+        )
+
+        del samples['observations']
+        del samples['next_observations']
+
+        return super(WeightedReplayPool, self).add_samples(samples)
+
+    def batch_by_indices(self,
+                         indices,
+                         field_name_filter=None,
+                         observation_keys=None):
+        if not isinstance(self._observation_space, Dict):
+            return super(WeightedReplayPool, self).batch_by_indices(
+                indices, field_name_filter=field_name_filter)
+
+        batch = {
+            field_name: self.fields[field_name][indices]
+            for field_name in self.field_names
+        }
+
+        if observation_keys is None:
+            observation_keys = tuple(self._observation_space.spaces.keys())
+
+        observations = np.concatenate([
+            batch['observations.{}'.format(key)]
+            for key in observation_keys
+        ], axis=-1)
+
+        next_observations = np.concatenate([
+            batch['next_observations.{}'.format(key)]
+            for key in observation_keys
+        ], axis=-1)
+
+        batch['observations'] = observations
+        batch['next_observations'] = next_observations
+
+        if field_name_filter is not None:
+            filtered_fields = self.filter_fields(
+                batch.keys(), field_name_filter)
+            batch = {
+                field_name: batch[field_name]
+                for field_name in filtered_fields
+            }
+        return batch
+
+    def terminate_episode(self):
+        pass
+
